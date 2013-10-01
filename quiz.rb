d547811ef453683956c0644e5ca85487ebd1a19f
Dan Crews <dancrews@me.com>
2013-10-01 09:36:47 -0400

    initial commit. Shell of spiral function.

--- quiz.rb
+++ quiz.rb
@@ -1,1 +1,51 @@
-# Your code goes here
+
+
+def spiral(array)
+
+# read_array
+array_holder = array
+
+# determine array_holder size
+n = array_holder.length
+m = array_holder[0].length
+
+#count number of indices in array holder
+
+
+# get first row
+
+# get last column
+
+# get last row
+# 	reverse last row
+
+# get first row
+# 	reverse first row
+# 	delete last element 
+
+
+
+
+end
+
+test_array = []
+#test_array = [[1,2,3],  [8,9,4],  [7,6,5]]
+
+spiral(test_array)
+
+
+
+
+
+# Write a function called "spiral" that takes an NxM array as input
+# and returns a "spiral" path through the array, starting in the
+# upper-left corner and moving clockwise. Use the language of your
+# choice.
+
+# For example (in Ruby), if
+
+# array = [[1,2,3],          [8,9,4],          [7,6,5]] then
+
+# spiral(array) => [1,2,3,4,5,6,7,8,9] It should work on any NxM
+# array, so don't assume the input is a square array. However, every
+# row will have the same number of elements.
