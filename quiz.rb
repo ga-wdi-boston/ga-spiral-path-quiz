7761bcd614e3f1ed212890813f28e5f87cdb8044
Rahul Horé <hore.rahul@gmail.com>
2013-10-01 10:01:57 -0400

    Refactors code

--- quiz.rb
+++ quiz.rb
@@ -1,5 +1,4 @@
 def spiral(two_d_array)
-  return two_d_array.flatten! if two_d_array.length < 2
   
   path = []
   spin = two_d_array.length + two_d_array[0].length - 1
@@ -9,5 +8,5 @@ def spiral(two_d_array)
     two_d_array = two_d_array.transpose.reverse
   end
 
-  path.flatten!
+  path.flatten.compact
 end
