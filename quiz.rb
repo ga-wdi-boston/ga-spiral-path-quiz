858512162e62b1a7c60b28be97dfb2ec6342117e
Rahul Horé <hore.rahul@gmail.com>
2013-10-01 09:27:31 -0400

    Should work for all matrices now

--- quiz.rb
+++ quiz.rb
@@ -1,1 +1,14 @@
-# Your code goes here
+def spiral(two_d_array)
+  return two_d_array.flatten! if two_d_array.length < 2
+  
+  path = []
+  spin = two_d_array.length + two_d_array[0].length - 1
+
+  spin.times do
+    path << two_d_array.delete_at(0)
+    two_d_array = two_d_array.transpose.reverse
+    puts path
+  end
+
+  path.flatten!
+end
