4b9c851001927c37247621b3c2971cc3c62de7fc
Cori Allen <coriallen@gmail.com>
2013-10-01 09:21:15 -0400

    Spiral solution.

--- quiz.rb
+++ quiz.rb
@@ -1,1 +1,10 @@
-# Your code goes here
+
+spiral = [[1,2,3],[4,5,6],[7,8,9]]
+spiral_solution = []
+spiral_solution << spiral.first
+spiral_solution << spiral[1][2]
+spiral_solution << spiral.last.reverse
+spiral_solution << spiral[1].first(2)
+p spiral_solution.flatten
+
+	
