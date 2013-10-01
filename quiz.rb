56fb0a51f28ce1b5e3424675554df48c72dba887
Matt Clement <darkmoves@gmail.com>
2013-10-01 10:35:02 -0400

    Committing changes.

--- quiz.rb
+++ quiz.rb
@@ -1,1 +1,20 @@
 # Your code goes here
+
+array = [
+	[1, 2, 3],
+	[8, 9, 4], 
+	[7, 6, 5]
+	]
+
+# This is a start on printing the array. Not sure where to go from here. 
+
+def spiral(array)
+	a = array
+	path = []
+
+	path << a[0]
+
+	p path
+end
+
+spiral(array)
