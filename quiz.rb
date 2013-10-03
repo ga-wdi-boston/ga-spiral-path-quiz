a1d3744e7cf440eed1a7a20215fbdd396daab5ef
Bobby Mazzotti <robert.mazzoti@gmail.com>
2013-10-03 07:06:39 -0400

    Final solution

--- quiz.rb
+++ quiz.rb
@@ -1,9 +1,31 @@
 # Your code goes here
-def spiral
-	a = [[1, 2, 3] [4, 5, 6] [7, 8, 9]]
-	abc = [a(0,1) a(0,2) a(0,3)] a(1,6) a(2,9)]
+def spiral(arr)
+  result = []
+  4.times { |x|
+    result[x] = []
+    4.times { |y|
+      result[x][y] = arr[y][3 - x]
+    }
+  }
 
-	end
+  result
 end
 
-puts Spiral.new(abc)
+arr = []
+arr[0] = [1,2,3,4]
+arr[1] = [5,6,7,8]
+arr[2] = [9,0,1,2]
+arr[3] = [3,4,5,6]
+
+def print_arr(arr)
+  4.times { |y|
+    4.times { |x|
+      print "#{arr[x][y]} "
+    }
+    puts ""
+  }
+end
+
+print_arr(arr)
+puts ""
+print_arr(spiral(arr))
