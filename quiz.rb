6b6e852e60ea23ef3faab8c7e0f0dcf100f3e1f4
Rahul Horé <hore.rahul@gmail.com>
2013-10-02 21:44:41 -0400

    Replaces delete_at(0) with shift

--- quiz.rb
+++ quiz.rb
@@ -1,7 +1,7 @@
 def spiral(two_d_array)
   path = []
   until two_d_array.empty?
-    path += two_d_array.delete_at(0)
+    path += two_d_array.shift
     two_d_array = two_d_array.transpose.reverse
   end
   path
